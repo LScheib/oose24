b1405b283adc69ea74a4413603507a8628cfd0a5
Ioannis Lilikakis <73935606+ililikakis@users.noreply.github.com>
2024-05-02 10:25:27 +0200

    Update src/homework_01/exercise_2/Main.java
    shorten code
    
    Co-authored-by: Lukas Scheib <146953413+LScheib@users.noreply.github.com>

--- src/homework_01/exercise_2/Main.java
+++ src/homework_01/exercise_2/Main.java
@@ -4,28 +4,22 @@ public class Main
 {
     public static void main(String [] args)
     {
-        System.out.println("member");
         UniMember member = new UniMember("Alice", "Wonderland");
-        System.out.println(member.getName());
+        System.out.println("University member:\n" + member.getName());
 
-        System.out.println("student");
         Student student = new Student("Alice", "Wonderland", "computer science");
-        System.out.println(student.getName() + ", " + student.getSubject());
+        System.out.println("Student:\n" + student.getName() + ", " + student.getSubject());
 
-        System.out.println("employee");
         Employee employee = new Employee("Alice", "Wonderland", 48000);
-        System.out.println(employee.getName() + ", " + employee.getSalary());
+        System.out.println("Employee:\n" + employee.getName() + ", " + employee.getSalary());
 
-        System.out.println("professor");
         Professor professor = new Professor("Alice", "Wonderland", 70000);
-        System.out.println(professor.getName() + ", " + professor.getSalary());
+        System.out.println("Professor:\n" + professor.getName() + ", " + professor.getSalary());
 
-        System.out.println("phd");
         PhD phd = new PhD("Alice", "Wonderland", 52000);
-        System.out.println(phd.getName() + ", " + phd.getSalary());
+        System.out.println("PhD:\n" + phd.getName() + ", " + phd.getSalary());
 
-        System.out.println("tutor");
         Tutor tutor = new Tutor("Alice", "Wonderland", "computer science", 8000);
-        System.out.println(tutor.getName() + ", " + student.getSubject() + ", " + tutor.getSalary());
+        System.out.println("Tutor:\n" + tutor.getName() + ", " + student.getSubject() + ", " + tutor.getSalary());
     }
 }
